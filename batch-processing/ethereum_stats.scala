fccec013a5b28bde44f623125db578d6fe3a4d7e
vladimirivkovic <vlado.ivkovic@live.com>
2020-06-14 01:00:42 +0200

    ethereum price for each block (join example)

--- batch-processing/ethereum_stats.scala
+++ batch-processing/ethereum_stats.scala
@@ -9,14 +9,32 @@ import org.apache.spark.sql.avro._
 val path = "hdfs://namenode:9000/user/data/eth-block/"
 val blocksDF = spark.read.format("avro").load(path)
 
-blocksDF.printSchema()
+// blocksDF.printSchema()
+
+val ratesPath = "hdfs://namenode:9000/user/data/exchange-rates/"
+val ratesDF = spark.read.format("avro").load(ratesPath)
+
+// ratesDF.printSchema()
+val filteredRatesDF = ratesDF.select("*").filter($"PRICE" > 0).filter($"LASTUPDATE" > 0)
+filteredRatesDF.createOrReplaceTempView("rates")
 
 blocksDF.createOrReplaceTempView("blocks")
 
-spark.sql("SELECT AVG(gasUsed) FROM blocks").show
-
-spark.sql("SELECT COUNT(*) FROM blocks").show
-
-spark.sql("SELECT COUNT(distinct miner) FROM blocks").show
+// spark.sql("SELECT AVG(gasUsed) FROM blocks").show
+// spark.sql("SELECT COUNT(*) FROM blocks").show
+// spark.sql("SELECT COUNT(distinct miner) FROM blocks").show
 
 blocksDF.withColumn("txs_no", size($"transactions")).agg(avg($"txs_no")).show
+
+spark.sql("SELECT hash, to_timestamp(timestamp), avg(PRICE) FROM blocks JOIN rates " + 
+          "ON bround(timestamp/60, 0) == bround(LASTUPDATE/60, 0) " + 
+          "WHERE FROMSYMBOL == 'ETH' GROUP BY hash, timestamp ORDER BY timestamp").show
+
+//.select("hash", to_timestamp($"timestamp"), "avg_price")
+filteredRatesDF
+    .filter($"FROMSYMBOL" === "ETH")
+    .join(blocksDF, bround($"LASTUPDATE"/60) === bround($"timestamp"/60))
+    .groupBy("hash", "timestamp")
+    .agg(avg($"PRICE").alias("avg_price"))
+    .orderBy("timestamp")
+    .show
